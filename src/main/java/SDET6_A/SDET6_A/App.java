12f15a5d88bfa6c82201cea68f9a60cb0a5f4b0f
anusha <anusha@LAPTOP-KB5AQG8N>
2022-05-06 10:43:41 +0530

    facebook and vtiger

---
+++ src/main/java/SDET6_A/SDET6_A/App.java
@@ -0,0 +1,13 @@
+package SDET6_A.SDET6_A;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
